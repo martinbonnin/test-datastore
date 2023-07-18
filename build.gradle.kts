4d84716dfc8fd946e9ece85da20c1818030f6e6f
Martin Bonnin <martin@mbonnin.net>
2023-07-18 20:37:46 +0200

    reproducer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
 plugins {
-  id("org.jetbrains.kotlin.jvm").version("1.9.0")
+  id("com.android.application").version("8.0.0")
+  id("org.jetbrains.kotlin.android").version("1.9.0")
+}
+
+android {
+  namespace = "sample"
+  compileSdk = 33
+}
+
+dependencies {
+  implementation("androidx.datastore:datastore-preferences:1.1.0-alpha04")
+  implementation("com.google.firebase:firebase-sessions:1.0.0")
 }
